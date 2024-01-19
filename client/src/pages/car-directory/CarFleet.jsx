775537bcf34cfd1133d27a35d3e9c89f9c1ee776
Shiva Dhakal <theskdhakal@gmail.com>
2024-01-19 14:52:19 +1100

    search function implemented in car fleet

--- client/src/pages/car-directory/CarFleet.jsx
+++ client/src/pages/car-directory/CarFleet.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { UserLayout } from "../../components/layout/UserLayout";
 import { Link } from "react-router-dom";
+import CustomCard from "../../components/custom-card/CustomCard";
+import CustomInput from "../../components/custom-input/CustomInput";
 
 const CarFleet = () => {
   const { cars } = useSelector((state) => state.carInfo);
@@ -10,60 +12,40 @@ const CarFleet = () => {
 
   const handleOnChange = (e) => {
     const { value } = e.target;
+
+    const filteredItem = cars.filter((item) =>
+      item.title.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setDisplay(filteredItem);
   };
-  console.log(cars);
+
+  useEffect(() => {
+    !display.length && setDisplay(cars);
+  }, [cars]);
+
+  console.log(display);
+
   return (
     <UserLayout>
       <h1 className="my-5 text-center underline text-red-600">
         Explore our Car Ranges
       </h1>
 
-      <div className="search mt-5 mb-2 flex justify-end mr-3">
-        <input type="search" width={50} className="border border-black" />
+      <div className="search mt-5 mb-2 flex justify-between mr-3">
+        <h5 className="mt-2 text-lg font-bold">{display.length} Cars found</h5>
+        <CustomInput
+          placeholder="Search car by title "
+          className="border border-black"
+          onChange={handleOnChange}
+        />
       </div>
 
       <hr />
 
       <div className="flex space-x-8 mt-5">
-        {cars.map((car, i) => (
-          <div
-            className="w-full max-w-sm bg-primary border border-gray-200 rounded-lg shadow "
-            key={i}
-          >
-            <Link to="#">
-              <img
-                className="p-8 rounded-t-lg"
-                src={car.image}
-                alt="product image"
-              />
-            </Link>
-            <div className="px-5 pb-5">
-              <Link to="#">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                  {car.title}
-                </h5>
-              </Link>
-              <div className="flex items-center mt-2.5 mb-5">
-                <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                  rating
-                </div>
-                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-                  5.0
-                </span>
-              </div>
-              <div className="flex items-center justify-between">
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  {car.price}
-                </span>
-                <Link
-                  to="#"
-                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Book Now
-                </Link>
-              </div>
-            </div>
-          </div>
+        {display.map((item) => (
+          <CustomCard key={item.id} {...item} />
         ))}
       </div>
     </UserLayout>
